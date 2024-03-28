33bc9ac44be3c70bef30ed452fb56e1b3a1e16be
Tesmi-Devlop <sashdfr@gmail.com>
2024-03-28 14:41:06 +1000

    Updated flamework 1.2.1

--- src/source/decorators/sharedClass.ts
+++ src/source/decorators/sharedClass.ts
@@ -2,6 +2,7 @@ import { AbstractConstructor, Constructor } from "@flamework/core/out/utility";
 import { SharedClasses } from "../Core";
 import { Shared } from "../Shared";
 
+/** @metadata reflect identifier flamework:parameters */
 export const SharedClass = () => {
 	return <T extends Shared>(classConstructor: AbstractConstructor<T>) => {
 		SharedClasses.RegisterySharedConstructor(classConstructor);
