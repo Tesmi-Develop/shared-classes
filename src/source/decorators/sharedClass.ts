a6f561e665b0b0cf4ef9c64be4ca68f46c9e8f80
Tesmi-Devlop <sashdfr@gmail.com>
2024-03-28 14:44:10 +1000

    Removed unnessary metadata

--- src/source/decorators/sharedClass.ts
+++ src/source/decorators/sharedClass.ts
@@ -2,7 +2,7 @@ import { AbstractConstructor, Constructor } from "@flamework/core/out/utility";
 import { SharedClasses } from "../Core";
 import { Shared } from "../Shared";
 
-/** @metadata reflect identifier flamework:parameters */
+/** @metadata identifier */
 export const SharedClass = () => {
 	return <T extends Shared>(classConstructor: AbstractConstructor<T>) => {
 		SharedClasses.RegisterySharedConstructor(classConstructor);
